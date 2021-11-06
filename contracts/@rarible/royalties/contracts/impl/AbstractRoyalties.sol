9a74d5785d789ba259344d79bc57802f2b73a3f4
Koji Miyata <calcio.cogi@gmail.com>
2021-11-06 12:20:05 +0900

    url

--- contracts/@rarible/royalties/contracts/impl/AbstractRoyalties.sol
+++ contracts/@rarible/royalties/contracts/impl/AbstractRoyalties.sol
@@ -1,32 +1,49 @@
-// SPDX-License-Identifier: MIT
-
+//SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "../LibPart.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./@rarible/royalties/contracts/impl/RoyaltiesV2Impl.sol";
+import "./@rarible/royalties/contracts/LibPart.sol";
+import "./@rarible/royalties/contracts/LibRoyaltiesV2.sol";
 
-abstract contract AbstractRoyalties {
-    mapping (uint256 => LibPart.Part[]) internal royalties;
+contract MinimalERC721 is ERC721, Ownable, RoyaltiesV2Impl {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdTracker;
 
-    function _saveRoyalties(uint256 id, LibPart.Part[] memory _royalties) internal {
-        uint256 totalValue;
-        for (uint i = 0; i < _royalties.length; i++) {
-            require(_royalties[i].account != address(0x0), "Recipient should be present");
-            require(_royalties[i].value != 0, "Royalty value should be positive");
-            totalValue += _royalties[i].value;
-            royalties[id].push(_royalties[i]);
-        }
-        require(totalValue < 10000, "Royalty total value should be < 10000");
-        _onRoyaltiesSet(id, _royalties);
+    bytes4 private constant _INTERFACE_ID_ERC2981 = 0x2a55205a;
+    constructor() ERC721("Minimal", "MIN") {}
+
+    function mint(address _to) public onlyOwner {
+        super._mint(_to, _tokenIdTracker.current());
+        _tokenIdTracker.increment();        
     }
 
-    function _updateAccount(uint256 _id, address _from, address _to) internal {
-        uint length = royalties[_id].length;
-        for(uint i = 0; i < length; i++) {
-            if (royalties[_id][i].account == _from) {
-                royalties[_id][i].account = payable(address(uint160(_to)));
-            }
-        }
+    function setRoyalties(uint _tokenId, address payable _royaltiesReceipientAddress, uint96 _percentageBasisPoints) public onlyOwner {
+        LibPart.Part[] memory _royalties = new LibPart.Part[](1);
+        _royalties[0].value = _percentageBasisPoints;
+        _royalties[0].account = _royaltiesReceipientAddress;
+        _saveRoyalties(_tokenId, _royalties);
     }
 
-    function _onRoyaltiesSet(uint256 id, LibPart.Part[] memory _royalties) virtual internal;
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) external view returns (address receiver, uint256 royaltyAmount) {
+        LibPart.Part[] memory _royalties = royalties[_tokenId];
+        if(_royalties.length > 0) {
+            return (_royalties[0].account, (_salePrice * _royalties[0].value)/10000);
+        }
+        return (address(0), 0);
+
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
+        if(interfaceId == LibRoyaltiesV2._INTERFACE_ID_ROYALTIES) {
+            return true;
+        }
+        if(interfaceId == _INTERFACE_ID_ERC2981) {
+            return true;
+        }
+        return super.supportsInterface(interfaceId);
+    } 
+
 }
