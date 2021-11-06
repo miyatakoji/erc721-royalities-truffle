3ae2229e3e8def2053a02df491ac97e8cef9e962
Koji Miyata <calcio.cogi@gmail.com>
2021-11-06 12:18:53 +0900

    apply for rarible secondary sales royalties

--- contracts/MinimalERC721.sol
+++ contracts/MinimalERC721.sol
@@ -4,8 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./@rarible/royalties/contracts/impl/RoyaltiesV2Impl.sol";
+import "./@rarible/royalties/contracts/LibPart.sol";
+import "./@rarible/royalties/contracts/LibRoyaltiesV2.sol";
 
-contract MinimalERC721 is ERC721, Ownable {
+contract MinimalERC721 is ERC721, Ownable, RoyaltiesV2Impl {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdTracker;
     constructor() ERC721("Minimal", "MIN") {}
@@ -14,4 +17,19 @@ contract MinimalERC721 is ERC721, Ownable {
         super._mint(_to, _tokenIdTracker.current());
         _tokenIdTracker.increment();        
     }
+
+    function setRoyalties(uint _tokenId, address payable _royaltiesReceipientAddress, uint96 _percentageBasisPoints) public onlyOwner {
+        LibPart.Part[] memory _royalties = new LibPart.Part[](1);
+        _royalties[0].value = _percentageBasisPoints;
+        _royalties[0].account = _royaltiesReceipientAddress;
+        _saveRoyalties(_tokenId, _royalties);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721) returns (bool) {
+        if(interfaceId == LibRoyaltiesV2._INTERFACE_ID_ROYALTIES) {
+            return true;
+        }
+        return super.supportsInterface(interfaceId);
+    } 
+
 }
