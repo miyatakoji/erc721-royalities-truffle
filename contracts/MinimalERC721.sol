27c7ca6d4e8156f454fd6fecccff024934e48ff0
Koji Miyata <calcio.cogi@gmail.com>
2021-11-06 11:26:35 +0900

    apply for opensea royalties

--- contracts/MinimalERC721.sol
+++ contracts/MinimalERC721.sol
@@ -2,8 +2,16 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MinimalERC721 is ERC721 {
-
+contract MinimalERC721 is ERC721, Ownable {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIdTracker;
     constructor() ERC721("Minimal", "MIN") {}
+
+    function mint(address _to) public onlyOwner {
+        super._mint(_to, _tokenIdTracker.current());
+        _tokenIdTracker.increment();        
+    }
 }
--- migrations/2_erc721.js
+++ migrations/2_erc721.js
@@ -3,3 +3,13 @@ const Token = artifacts.require("MinimalERC721");
 module.exports = function (deployer) {
   deployer.deploy(Token);
 };
+
+// module.exports = function (deployer, network) {
+//   console.log(network);
+//   if (network != "live") {
+//     deployer.exec("add_demo_data.js");
+//   }
+
+//   var old_contract = eth.contract(abi).at(address1);
+//   old_contract.upgrade.sendTransaction(address2);
+// };
